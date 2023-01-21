c7722784683ca752355a9517f56d17c314f16c15
tgondil <tgondil@purdue.edu>
2023-01-21 03:24:24 -0500

    Added ability to switch between maps

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
-import pandas as pd
+import plotly.graph_objects as go
 import plotly.express as px
+import pandas as pd
 
 # Read data from an xlsx file
 df = pd.read_excel('data.xlsx', header=3)
 
-fig = px.scatter_geo(df, lat="Latitude", lon="Longitude", scope='usa')
+# Create a scatter map
+fig = px.scatter_geo(df, lat="Latitude", lon="Longitude", scope='usa', color = 'Total reported direct emissions')
+
+# Add a button to switch between showing carbon and nitrogen pollution
+fig.update_layout(
+    updatemenus=[
+        go.layout.Updatemenu(
+            active=0,
+            buttons=list([
+                dict(label="Carbon",
+                     method="update",
+                     args=[{"visible": [True, False]},
+                           {"title": "Carbon Pollution"}]),
+                dict(label="Nitrogen",
+                     method="update",
+                     args=[{"visible": [False, True]},
+                           {"title": "Nitrogen Pollution"}]),
+            ]),
+        )
+    ]
+)
+
 fig.show()
