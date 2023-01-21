d20e4e3ded72857c844a5f248fdb3ef191481620
AshwinDesh <112188799+AshwinDesh@users.noreply.github.com>
2023-01-21 13:28:48 -0500

    Update main.py

--- main.py
+++ main.py
@@ -38,14 +38,42 @@
 
 #fig.show()
 import pandas as pd
+import plotly.graph_objects as go
 df = pd.read_excel('data.xlsx', header=3)
 
 import plotly.express as px
 
 fig = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", hover_name="City", hover_data=["State", "County"],
-                        color_discrete_sequence=["fuchsia"], zoom=3, height=750)
+                        color_discrete_sequence=['#EF553B'], zoom=3, height=750)
+figcar = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", hover_name="City", hover_data=["State", "County"],
+                        color='CO2 emissions (non-biogenic) ', zoom=3, height=750)
+fignit = px.scatter_mapbox(df, lat="Latitude", lon="Longitude", hover_name="City", hover_data=["State", "County"],
+                        color='Nitrous Oxide (N2O) emissions ', zoom=3, height=750)
+figcar.update_traces(visible=False)
+fignit.update_traces(visible=False)
 
-fig.update_layout(mapbox_style="dark", mapbox_accesstoken='pk.eyJ1IjoiYXNod2luZGVzaCIsImEiOiJjbGQ2Nm9jZ2UwZHhyM3FzZGhmZ2U5bGNrIn0.ShkpAMGCM3RNz0SX3If1CQ')
+fig.add_trace(figcar['data'][0])
+fig.add_trace(fignit['data'][0])
+
+fig.update_layout(mapbox_style="light", mapbox_accesstoken='pk.eyJ1IjoiYXNod2luZGVzaCIsImEiOiJjbGQ2Nm9jZ2UwZHhyM3FzZGhmZ2U5bGNrIn0.ShkpAMGCM3RNz0SX3If1CQ', margin={"r":0,"t":0,"l":0,"b":0},)
 fig.update_traces(cluster=dict(enabled=True))
 
+fig.update_layout(
+    updatemenus=[
+        go.layout.Updatemenu(
+            active=0,
+            type = 'buttons',
+            buttons=list([
+                dict(label="Carbon",
+                     method="update",
+                     args=[{"visible": [False, True]},
+                           {"title": "Carbon Pollution"}]),
+                dict(label="Nitrogen",
+                     method="update",
+                     args=[{"visible": [True, False]},
+                           {"title": "Nitrogen Pollution"}]),
+            ]),
+        )
+    ]
+)
 fig.show()
