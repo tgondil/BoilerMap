d16105bc57217f926773fa587fbae9a82bbebdb6
tgondil <tgondil@purdue.edu>
2023-01-21 18:31:42 -0500

    Added death

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 import pandas as pd
 import plotly.graph_objects as go
+from urllib.request import urlopen
+import json
+with urlopen('https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json') as response:
+    counties = json.load(response)
 df = pd.read_excel('data.xlsx', header=3)
+df2 = pd.read_csv('death.csv',
+                   dtype={"fips": str}, header = 4)
+
+counties["features"][0]
 
 import plotly.express as px
 
@@ -16,6 +24,19 @@ hfc = px.scatter_mapbox(df.dropna(subset=['HFC emissions']), lat="Latitude", lon
                         color='HFC emissions', zoom=3, height=750)
 sf = px.scatter_mapbox(df.dropna(subset=['SF6 emissions']), lat="Latitude", lon="Longitude", hover_name="City", hover_data=["State", "County", "Industry Sector"],
                         color='SF6 emissions', zoom=3, height=750)
+#death = px.choropleth_mapbox(df2, geojson=counties, locations=' FIPS', color='Average Annual Count', zoom=3, height=750,
+#mapbox_style="carto-positron", center={"lat": 33.33012299999999, "lon": -81.08463033333332},)
+
+#death.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+
+death = px.choropleth_mapbox(df2, geojson=counties, locations='fips', color='Count',
+                           color_continuous_scale="Viridis",
+                           range_color=(0, 12),
+                           mapbox_style="carto-positron",
+                           zoom=3, center = {"lat": 37.0902, "lon": -95.7129},
+                           opacity=0.5,
+                           labels={'unemp':'unemployment rate'}
+                          )
 
 
 fig.add_trace(carbon['data'][0])
@@ -57,8 +78,13 @@ fig.update_layout(
                     method="update",
                     args=[{"visible": [False, False, False, False, False, True]},
                             {"title": "SF6 Pollution"}]),
+                dict(label="Age Adjusted Death Rate",
+                     method="update",
+                     args=[{"visible": [False, False, False, False, False, False]},
+                           {"title": "Age Adjusted Death Rate"}]),
             ]),
         )
     ]
 )
 fig.show()
+death.show()
