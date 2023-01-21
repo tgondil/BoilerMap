bf15cfc5cc8fbdf023250abf5690f9dfeb41ff2d
tgondil <tgondil@purdue.edu>
2023-01-21 03:01:29 -0500

    Turned into a scatter plot.

--- main.py
+++ main.py
@@ -1,19 +1,8 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import plotly.express as px
 
 # Read data from an xlsx file
 df = pd.read_excel('data.xlsx', header=3)
 
-# Group the data by 'State' and calculate the sum of 'CO2 emissions (non-biogenic)' for each group
-df_grouped = df.groupby('State')['CO2 emissions (non-biogenic) '].sum().reset_index()
-
-# Create a Choropleth map of the United States
-fig = px.choropleth(df_grouped,
-                    locations='State',  # Column containing the state names
-                    locationmode='USA-states',  # Set the location mode to USA-states
-                    color='CO2 emissions (non-biogenic) ',  # Column containing the data to be plotted
-                    color_continuous_scale='Viridis',  # Color scale to use
-                    title='CO2 emissions (non-biogenic) by State',
-                    scope='usa')
+fig = px.scatter_geo(df, lat="Latitude", lon="Longitude", scope='usa')
 fig.show()
