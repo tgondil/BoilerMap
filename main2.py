5c0b90c5b1ae9fd20d653c20f8acd2c63d498f71
tgondil <112219615+tgondil@users.noreply.github.com>
2023-01-22 01:10:35 -0500

    For the heatwaves

---
+++ main2.py
@@ -0,0 +1,32 @@
+import pandas as pd
+import plotly.graph_objects as go
+dfhw = pd.read_csv('heatwaves.csv')
+
+import plotly.express as px
+
+fig = px.density_mapbox(dfhw, lat='Latitude', lon='Longitude', z='Intensity Change', radius=50, center=dict(lat=39.49, lon=-98.95734), zoom =3, mapbox_style="stamen-terrain",)
+fig.update_layout(mapbox_style="light", mapbox_accesstoken='pk.eyJ1IjoiYXNod2luZGVzaCIsImEiOiJjbGQ2Nm9jZ2UwZHhyM3FzZGhmZ2U5bGNrIn0.ShkpAMGCM3RNz0SX3If1CQ', margin={"r":0,"l":0,"b":0},)
+duration = px.density_mapbox(dfhw.dropna(subset=['Duration Change']), lat='Latitude', lon='Longitude', z='Duration Change', radius=50, center=dict(lat=39.49, lon=-98.95734), zoom =3, mapbox_style="stamen-terrain", labels={'Duration Change'})
+fig.add_trace(duration['data'][0])
+
+fig.update_layout(
+    updatemenus=[
+        go.layout.Updatemenu(
+            active=0,
+            type = 'dropdown',
+            y=0.9,
+            buttons=list([
+                dict(label="Intensity Change in Heat Waves",
+                     method="update",
+                     args=[{"visible": [True, False]},
+                           {"title": "Intensity Change"}]),
+
+                dict(label="Duration Change in Heat Waves",
+                     method="update",
+                     args=[{"visible": [False, True]},
+                           {"title": "Duration Change"}]),
+                          ]),
+        ),
+    ]
+)
+fig.show()
